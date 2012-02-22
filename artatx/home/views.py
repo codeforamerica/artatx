0d270ad8d2e82d0bfcff7ee5a1e528ad58e97f25
Aurelio Tinio <aurelio@tinio.org>
2012-02-22 17:57:02 -0600

    artwork index page

--- artatx/home/views.py
+++ artatx/home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from artworks.models import Artwork
+
 def index(request, template_name='home/index.html'):
     context = {}
+    context['artworks'] = Artwork.objects.all()
     return render_to_response(template_name, context,
         context_instance=RequestContext(request))
