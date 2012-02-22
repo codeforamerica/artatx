b7954203dc5a535a7444c9ce0cb07d35b1f649fd
Aurelio Tinio <aurelio@tinio.org>
2012-02-22 17:45:30 -0600

    add homepage view

--- artatx/urls.py
+++ artatx/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'home.views.index', name='home_index'),
 )
