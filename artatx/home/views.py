5d5138230ce93e0de7824f73cdf159da6c4cb892
Aurelio Tinio <aurelio@tinio.org>
2012-02-22 17:48:22 -0600

    typo

--- artatx/home/views.py
+++ artatx/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def index(request, template_name='home/index.html')
+def index(request, template_name='home/index.html'):
     context = {}
     return render_to_response(template_name, context,
         context_instance=RequestContext(request))
