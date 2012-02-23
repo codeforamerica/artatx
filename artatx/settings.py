d49473d2b1b085dc5e3bb6d1c97c251834512911
Aurelio Tinio <aurelio@tinio.org>
2012-02-23 12:24:38 -0600

    specify localhost ip 127.0.0.1

--- artatx/settings.py
+++ artatx/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'artatx',                      # Or path to database file if using sqlite3.
         'USER': 'cfa',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
