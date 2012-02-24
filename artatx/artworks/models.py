c4c28f80120c59aa9cadb5638739fc0175e0095b
Aurelio Tinio <aurelio@tinio.org>
2012-02-23 21:23:00 -0600

    add level field

--- artatx/artworks/models.py
+++ artatx/artworks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Artwork(models.Model):
+    level = models.SmallIntegerField()
     work_num = models.CharField(max_length=10)
     artist = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
