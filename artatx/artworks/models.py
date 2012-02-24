a2d97487311ea2d36a299e0d13fce5d35adda3f7
Aurelio Tinio <aurelio@tinio.org>
2012-02-23 21:21:54 -0600

    add additional fields

--- artatx/artworks/models.py
+++ artatx/artworks/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Artwork(models.Model):
-    title = models.CharField(max_length=255)
+    work_num = models.CharField(max_length=10)
     artist = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     year = models.SmallIntegerField()
-    website = models.URLField(max_length=255)
+    medium = models.CharField(max_length=255)
+    credit = models.CharField(max_length=255)
+    website = models.URLField(max_length=255, null=True, blank=True)
 
     def __unicode__(self):
         return self.title
